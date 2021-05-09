d13d0bd0fbf519867548828b68e2cd3209e35339
mario <mshields@cshl.edu>
2021-05-08 23:55:06 -0500

    HW sub

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,48 +3,56 @@ import csv
 
 # Path to collect data from Pybank
 Pybank = os.path.join("..", "Pybank", "Resources", "budget_data.csv")
-count = 0
 
-#def summary (Pybank): 
-    #profit_loss = int(Pybank[1])
-    #total = 0
-    #total = sum(profit_loss)
+#Lists
+profit_loss = []
+months = []
+monthly_changes = []
+month_count = 0
+total_profits = 0
+first_month = 0
+total_monthly = 0
 with open(Pybank, 'r') as csvfile:
 
     csv_reader = csv.reader(csvfile, delimiter=',')
 
     header = next(csv_reader)
-    total = 0
+    
     for row in csv_reader:
-        count += 1
-        total += int(row[1])
-print('Financial Analysis')
-print ('Total Month:',count) 
-print ('Total:', total)
-
-#with open(Pybank, 'r') as count_file:
-   # csv_reader = csv.reader(count_file)
-    #csv_header = next(csv_reader)
-    #for row in csv_reader:
-        #count += 1
-#print (count)
-
-#print('Pybank lives at:', Pybank)
-
-#def summary(Pybank):
-#date = str(Pybank[0])
-    #return (len(date))
-    #profit_loss = int(Pybank[1])
-    #Sum_months = len(1 for row in PyPoll)
-    
-#def summary(Pybank):
-    #profit_loss = int(Pybank[1])
-    #Sum = sum(profit_loss)
-    #print(Sum)
-
-    
-    #print(csvreader)
-    #csv_header = next(csvreader)
-    #print(csv_header)
-
+        month_count += 1
+        total_profits += int(row[1])
+        #store months in list
+        months.append(row[0])
+        #store profit_loss in list
+        profit_loss.append(row[1])
         
+        #average change from month to month
+        next_month = int(row[1])
+        monthly_change = next_month - first_month
+        #store monthly change to list profit_loss
+        monthly_changes.append(monthly_change)
+        #total monthly_change profits
+        total_monthly = total_monthly + monthly_change
+        first_month = next_month
+        average_monthly_change = (total_monthly/month_count)
+        greatest_increase = max(monthly_changes)
+        greatest_decrease = min(monthly_changes)
+        date_max = months[monthly_changes.index(greatest_increase)]
+        date_min = months[monthly_changes.index(greatest_decrease)]
+print("Financial Analysis")
+print(f"Total Month: {month_count}") 
+print(f"Total Profits:${total_profits}")
+print(f"Average Change: ${round(average_monthly_change,2)}")
+print(f"Greatest Increase in Profits:${greatest_increase} on {date_max}")
+print(f"Greatest Decrease in Profits:${greatest_decrease} on {date_min}")
+
+# Create a text file with the results
+output_file = 'Analysis/financial_analysis.txt'
+with open(output_file, "w", newline="") as datafile:
+    csvwriter = csv.writer(datafile)
+    csvwriter.writerow("Financial Analysis")
+    csvwriter.writerow(f"Total Months: {month_count}")
+    csvwriter.writerow(f"Total: ${total_profits}")
+    csvwriter.writerow(f"Average Change: ${round(average_monthly_change,2)}")
+    csvwriter.writerow(f"Greatest Increase in Profits: ${greatest_increase} on {date_max}")
+    csvwriter.writerow(f"Greatest Decrease in Profits: ${greatest_decrease} on {date_min}")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,11 +4,24 @@ import csv
 PyPoll = os.path.join("..", "PyPoll", "Resources", "election_data.csv")
 print('PyPoll lives at:', PyPoll)
 
-
-def vote_counts(PyPoll):
-    Voter_ID = str(PyPoll[0])
-    County = str(PyPoll[1])
-    Candidate = str(PyPoll[2])
-    
+candidates = {}
+vote_count = []
+total_votes = 0
+vote_count = 0
 with open(PyPoll, 'r') as csv_file:
-    csvreader = csv.reader(csv_file, delimiter=',',)
+    csv_reader = csv.reader(csv_file, delimiter=',',)
+    header = next(csv_reader)
+    
+    for row in csv_reader:
+        total_votes += 1
+        
+        #vote count per candidate
+        candidate = row[2]
+        #add to candidate list
+        candidates.append(candidate)
+        if candidate in candidates:
+            candidate += 1
+        else:
+            candidate = 1
+print(total_votes)
+print(candidate)
